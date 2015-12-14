3007b746989bae4d7736da740561670d88bb571a
JasonM17 <radical.jay007@gmail.com>
2015-12-14 16:20:28 -0500

    findBin() and findLin() update

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -54,15 +54,15 @@ public class OrderedArrayList {
     // uses a linear search to find appropriate index
     public void addLinear( Comparable newVal ) 
     { 
-				for( int p = 0; p < _data.size(); p++ ) {
-						if ( newVal.compareTo( _data.get(p) ) < 0 ) { //newVal < oal[p]
-								_data.add( p, newVal );
-								return; //Q:why not break?
-						}
-				}
-				_data.add( newVal ); //newVal > every item in oal, so add to end
+	for( int p = 0; p < _data.size(); p++ ) {
+	    if ( newVal.compareTo( _data.get(p) ) < 0 ) { //newVal < oal[p]
+		_data.add( p, newVal );
+		return; //Q:why not break?
+	    }
+	}
+	_data.add( newVal ); //newVal > every item in oal, so add to end
     }
-
+    
 
     // addBinary takes as input any comparable object 
     // (i.e., any object of a class implementing interface Comparable)
@@ -70,102 +70,102 @@ public class OrderedArrayList {
     // maintains ascending order of elements
     // uses a binary search to find appropriate index
     public void addBinary( Comparable newVal ) { 
-				//initialize upperbound, lowerbound and median
-				int lo = 0;
-				int med = 0;
-				int hi = _data.size()-1;
-
-				while ( lo <= hi ) { //running until target is found or bounds cross
-
-						med = (lo + hi) / 2;
-						int x = _data.get(med).compareTo( newVal );
-	        
-						if ( x == 0 ) { //equal value found, insert here
-								_data.add( med, newVal );
-								return;
-						}
-						else if ( x > 0 ) //newVal < med, so look at lower half of data
-								hi = med - 1;
-						else //newVal > med, so look at upper half of data
-								lo = med + 1;
-				}
-				// If you make it this far, newVal was not in the ArrayList.
-				// So insert at lo. Q: How do you know lo is correct insertion index?
-				_data.add( lo, newVal );
+	//initialize upperbound, lowerbound and median
+	int lo = 0;
+	int med = 0;
+	int hi = _data.size()-1;
+	
+	while ( lo <= hi ) { //running until target is found or bounds cross
+	    
+	    med = (lo + hi) / 2;
+	    int x = _data.get(med).compareTo( newVal );
+	    
+	    if ( x == 0 ) { //equal value found, insert here
+		_data.add( med, newVal );
+		return;
+	    }
+	    else if ( x > 0 ) //newVal < med, so look at lower half of data
+		hi = med - 1;
+	    else //newVal > med, so look at upper half of data
+		lo = med + 1;
+	}
+	// If you make it this far, newVal was not in the ArrayList.
+	// So insert at lo. Q: How do you know lo is correct insertion index?
+	_data.add( lo, newVal );
     }	
-
-
-		// determine whether element present in data structure using linear search
-		// return index of occurrence or -1 if not found
-		public int findLin( Comparable target ) 
-		{ 
-				/* YOUR IMPLEMENTAITON AQUI */
-		}
-
-
-		// determine whether element present in data structure using binary search
-		// return index of occurrence or -1 if not found
-    public int findBin( Comparable target ) 
-    { 
-				/* YOUR IMPLEMENTAITON AQUI */
-		}
-
-
-    // findBin()
-    public int finBin(int newVal, int lo, int hi) {
+    
+    
+    // determine whether element present in data structure using linear search
+    // return index of occurrence or -1 if not found
+    public int findLin( Comparable target ) {
+	for (int ctr = 0; ctr < _data.size(); ctr++){
+	    if (_data[ctr].compareTo(target) == 0) {return ctr;}
+	}
+	return -1;
+	
+    }
+    
+    
+    // determine whether element present in data structure using binary search
+    // return index of occurrence or -1 if not found
+    public int findBin( Comparable target ) {
+	int lo = 0;
+	int med = 0;
+	int hi = _data.size()-1;
+	return findBinH( target, lo, med, hi);
+    }	
+    
+    public int findBinH( Comparable target, int lo, int med, int hi){
 	if (lo < hi) {
 	    int med = (lo / 2) + (hi / 2);
 	    int comp = _data[med].compareTo(newVal);
 	    if (comp < 0) {
-		return binarySearch(newVal, lo, med - 1);}
+		return findBinH(newVal, lo, med - 1);}
 	    if (comp > 0) {
-		return binarySearch(newVal,  med + 1, hi);}
+		return findBinH(newVal,  med + 1, hi);}
 	    return med;
 	}
 	return -1; //covers appending first digit 
     } // binarySearch
-
-
-
-
+    
     // main method solely for testing purposes
     public static void main( String[] args ) 
     {
-				OrderedArrayList Franz = new OrderedArrayList();
-
-				System.out.println("\nValues to add via addLinear() calls:");
-
-				// testing linear search
-				for( int i = 0; i < 15; i++ ) {
-						int valToAdd = (int)( 50 * Math.random() );
-						System.out.println( valToAdd );
-						Franz.addLinear( valToAdd );
-				}
-
-				System.out.println("\nafter population via addLinear() calls:");
-				System.out.println( Franz );
-				System.out.println();
-
-				Franz = new OrderedArrayList();
-
-				System.out.println("\nValues to add via addBinary() calls:");
-
-				
-				// testing binary search
-				for( int i = 0; i < 15; i++ ) {
-						int valToAdd = (int)( 50 * Math.random() );
-						System.out.println( valToAdd );
-						Franz.addBinary( valToAdd );
-				}
-
-				System.out.println("\nafter population via addBinary() calls:");
-				System.out.println( Franz );
-				System.out.println();
-
-				/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-					 INSERT WELL-COMMENT TIMING APPARATUS HERE
-					 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	OrderedArrayList Franz = new OrderedArrayList();
+	
+	System.out.println("\nValues to add via addLinear() calls:");
+	
+	// testing linear search
+	for( int i = 0; i < 15; i++ ) {
+	    int valToAdd = (int)( 50 * Math.random() );
+	    System.out.println( valToAdd );
+	    Franz.addLinear( valToAdd );
+	}
+	
+	System.out.println("\nafter population via addLinear() calls:");
+	System.out.println( Franz );
+	System.out.println();
+	
+	Franz = new OrderedArrayList();
+	
+	System.out.println("\nValues to add via addBinary() calls:");
+	
+	
+	// testing binary search
+	for( int i = 0; i < 15; i++ ) {
+	    int valToAdd = (int)( 50 * Math.random() );
+	    System.out.println( valToAdd );
+	    Franz.addBinary( valToAdd );
+	}
+	
+	System.out.println("\nafter population via addBinary() calls:");
+	System.out.println( Franz );
+	System.out.println();
+	
+	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	   INSERT WELL-COMMENT TIMING APPARATUS HERE
+	   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     }
-
+    
 }//end class OrderedArrayList
- 
+
